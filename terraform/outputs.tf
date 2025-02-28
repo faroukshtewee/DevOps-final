84d913e98d0af1ba96c957bd26fa68de81f3b062
faroukshtewee <farok95@hotmail.com>
2025-03-01 01:32:46 +0200

    terraform edited

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,28 +1,54 @@
-output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main_vpc.id
+# EKS Outputs
+output "eks_cluster_id" {
+  description = "EKS cluster ID"
+  value       = module.eks.cluster_id
 }
 
-output "eks_cluster_name" {
-  description = "EKS Cluster Name"
-  value       = aws_eks_cluster.my_eks.name
+output "eks_cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
 }
 
-output "ecr_repository_url_init" {
-  description = "ECR Repository URL"
-  value       = aws_ecr_repository.my_ecr_init.repository_url
-}
-output "ecr_repository_url_django" {
-  description = "ECR Repository URL"
-  value       = aws_ecr_repository.my_ecr_django.repository_url
-}
-output "s3_bucket_url" {
-  value = "https://${aws_s3_bucket.my_s3_bucket.bucket}.s3.amazonaws.com"  # ✅ Corrected reference
+output "eks_cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = module.eks.cluster_security_group_id
 }
 
+output "eks_config_map_aws_auth" {
+  description = "A kubernetes configuration to authenticate to this EKS cluster"
+  value       = module.eks.aws_auth_configmap_yaml
+  sensitive   = true
+}
 
+output "eks_node_groups" {
+  description = "EKS node groups"
+  value       = module.eks.eks_managed_node_groups
+}
 
-output "cloudfront_url" {
-  description = "CloudFront Distribution URL"
-  value       = aws_cloudfront_distribution.my_distribution.domain_name
+# ECR Outputs
+output "ecr_repository_urls" {
+  description = "URLs of the ECR repositories"
+  value       = aws_ecr_repository.repositories[*].repository_url
+}
+
+# S3 Outputs
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.static_assets.id
+}
+
+output "s3_bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.static_assets.arn
+}
+
+# CloudFront Outputs
+output "cloudfront_distribution_id" {
+  description = "ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.s3_distribution.id
+}
+
+output "cloudfront_distribution_domain_name" {
+  description = "Domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.s3_distribution.domain_name
 }
