f1d8460ca53876a26775a27bd7720920550038f3
faroukshtewee <farok95@hotmail.com>
2025-03-02 21:45:09 +0200

    edit import_data.sh

--- init/import_data.sh
+++ init/import_data.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 DIRECTORY=./shared
 DONE_FILE_NAME=migration.done
-
 # Ensure directory exists
 if [ ! -d "$DIRECTORY" ]; then
   echo "Creating directory: $DIRECTORY"
   mkdir -p "$DIRECTORY"
 fi
-
 # Check if migration has already been done
 echo "Checking if $DIRECTORY/$DONE_FILE_NAME exists"
 if [ ! -f "$DIRECTORY/$DONE_FILE_NAME" ]; then
