fec6cba6259d36674adcf653e961849fa87cd5b5
faroukshtewee <farok95@hotmail.com>
2025-03-02 22:12:16 +0200

    edit import_data.sh

--- init/import_data.sh
+++ init/import_data.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 DIRECTORY=./shared
 DONE_FILE_NAME=migration.done
-# Ensure directory exists
 if [ ! -d "$DIRECTORY" ]; then
   echo "Creating directory: $DIRECTORY"
   mkdir -p "$DIRECTORY"
 fi
-# Check if migration has already been done
 echo "Checking if $DIRECTORY/$DONE_FILE_NAME exists"
 if [ ! -f "$DIRECTORY/$DONE_FILE_NAME" ]; then
-    echo "Start MySQL DB migration"
-    mysql -h 172.20.167.246 -u root -p"$SQL_ROOT_PASSWORD" < data.sql
-    touch "$DIRECTORY/$DONE_FILE_NAME"
+  echo "Start MySQL DB migration"
+  mysql -h 172.20.167.246 -u root -p"$SQL_ROOT_PASSWORD" < data.sql
+  touch "$DIRECTORY/$DONE_FILE_NAME"
 else
-    echo "Migration done file does not exist."
+  echo "Migration done file does not exist."
 fi
